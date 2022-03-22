a2ad0c5cb4313569dcad903235673a2c55729d9e
Frank <chenmaofrank1996@gmail.com>
2022-03-22 17:58:14 +0200

    Start building the profile page

--- app/src/main/java/com/example/mobile_development_project_group_1/ProfilePageView.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/ProfilePageView.kt
@@ -2,6 +2,7 @@ package com.example.mobile_development_project_group_1
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import com.google.firebase.auth.ktx.auth
@@ -10,50 +11,18 @@ import com.google.firebase.ktx.Firebase
 
 @Composable
 fun ProfilePageView() {
-    val fireStore = Firebase.firestore
-    val fAuth = Firebase.auth
-
-    var currentUserFirstName by remember { mutableStateOf("") }
-    var currentUserLastName by remember { mutableStateOf("") }
-    var currentUserAddress by remember { mutableStateOf("") }
-    var currentUserPhoneNumber by remember { mutableStateOf("") }
-
-    var firstName by remember { mutableStateOf("") }
-    var lastName by remember { mutableStateOf("") }
-    var address by remember { mutableStateOf("") }
-    var phoneNumber by remember { mutableStateOf("") }
-
-    fireStore
-        .collection("users")
-        .document(fAuth.currentUser?.uid.toString())
-        .get()
-        .addOnSuccessListener {
-            currentUserFirstName = it.get("firstName").toString()
-            currentUserLastName = it.get("lastName").toString()
-            currentUserAddress = it.get("address").toString()
-            currentUserPhoneNumber = it.get("phoneNumber").toString()
+   var isEdit :MutableState<Boolean> = remember {
+       mutableStateOf(true)
+   }
+    
+    if (isEdit.value){
+       Surface() {
+          Text(text = "Info-page")
+        }
+    }else{
+        Surface() {
+            Text(text = "Edit information page")
         }
 
-    Column {
-        OutlinedTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            placeholder = { Text(text = currentUserFirstName) }
-        )
-        OutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            placeholder = { Text(text = currentUserLastName) }
-        )
-        OutlinedTextField(
-            value = address,
-            onValueChange = { address = it },
-            placeholder = { Text(text = currentUserAddress) }
-        )
-        OutlinedTextField(
-            value = phoneNumber,
-            onValueChange = { phoneNumber = it },
-            placeholder = { Text(text = currentUserPhoneNumber) }
-        )
     }
 }
