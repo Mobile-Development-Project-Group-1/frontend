758255ba7416d2709288ff62295640f5332aba09
Frank <chenmaofrank1996@gmail.com>
2022-03-22 20:00:27 +0200

    update the layout profile page

--- app/src/main/java/com/example/mobile_development_project_group_1/ProfilePageView.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/ProfilePageView.kt
@@ -1,17 +1,21 @@
 package com.example.mobile_development_project_group_1
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
 
 import androidx.compose.ui.unit.dp
 
@@ -27,7 +31,11 @@ fun ProfilePageView() {
         Column(modifier = Modifier
             .fillMaxSize()
             .padding(10.dp), horizontalAlignment =Alignment.CenterHorizontally ) {
-         Surface(modifier = Modifier.width(295.dp).height(279.dp), shape = RoundedCornerShape(corner = CornerSize(15.dp)), color = Color.Black ) {
+            Text(text = "Your Profile", style = MaterialTheme.typography.h5, fontWeight = FontWeight.Bold)
+         Surface(modifier = Modifier
+             .width(295.dp)
+             .height(400.dp).padding(0.dp,10.dp), shape = RoundedCornerShape(corner = CornerSize(15.dp)),  border = BorderStroke(2.dp, Color.LightGray)
+         ) {
 
           }
 
