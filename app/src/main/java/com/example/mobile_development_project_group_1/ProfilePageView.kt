d0a5a1e6d59ffdf7486c6fa4c2b830d026b5f7e8
Frank <chenmaofrank1996@gmail.com>
2022-03-22 18:18:36 +0200

    Start layout the profile page

--- app/src/main/java/com/example/mobile_development_project_group_1/ProfilePageView.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/ProfilePageView.kt
@@ -1,10 +1,13 @@
 package com.example.mobile_development_project_group_1
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -16,12 +19,12 @@ fun ProfilePageView() {
    }
     
     if (isEdit.value){
-       Surface() {
-          Text(text = "Info-page")
+       Surface(modifier = Modifier.fillMaxSize(), color = Color.Green) {
+
         }
     }else{
-        Surface() {
-            Text(text = "Edit information page")
+        Surface(modifier = Modifier.fillMaxSize(), color = Color.Red) {
+
         }
 
     }
