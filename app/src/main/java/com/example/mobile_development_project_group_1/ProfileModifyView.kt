becec2fcf03134f196e38a13d81b4c742c7f9b9b
Frank <chenmaofrank1996@gmail.com>
2022-03-28 18:10:29 +0300

    modify  my code

--- app/src/main/java/com/example/mobile_development_project_group_1/ProfileModifyView.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/ProfileModifyView.kt
@@ -1,10 +1,10 @@
 package com.example.mobile_development_project_group_1
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
+
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -13,18 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+
 
 @Composable
 fun ProfileMOView(nav: NavHostController) {
