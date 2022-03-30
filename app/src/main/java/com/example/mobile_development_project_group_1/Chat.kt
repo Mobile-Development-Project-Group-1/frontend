34331d98f0169f641a7615273b5eebe8ae9cf92e
Frank <chenmaofrank1996@gmail.com>
2022-03-31 01:05:39 +0300

    Modifying and checking code

--- app/src/main/java/com/example/mobile_development_project_group_1/Chat.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/Chat.kt
@@ -1,6 +1,6 @@
 package com.example.mobile_development_project_group_1
 
-import androidx.compose.runtime.Composable
+
 import java.util.*
 
 data class Message(val msg:String, val username:String, val created_at: Date)
