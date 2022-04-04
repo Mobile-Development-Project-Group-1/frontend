1e8f314f9ebbffd53c6e279be05267b75c9d6556
Frank <chenmaofrank1996@gmail.com>
2022-04-04 16:58:43 +0300

    add new layout

--- app/src/main/java/com/example/mobile_development_project_group_1/PubPlaceCreationView.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/PubPlaceCreationView.kt
@@ -1,15 +1,20 @@
 package com.example.mobile_development_project_group_1
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
@@ -47,8 +52,77 @@ fun AddNewPubPlaceView(navController: NavHostController) {
                 }
             }
         } // Back to HomePage button
-        Column() {
-            Text(text = "CREATION PAGE")
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally ) {
+            Surface(modifier = Modifier
+                .width(295.dp)
+                .height(400.dp)
+                .padding(0.dp, 10.dp), shape = RoundedCornerShape(corner = CornerSize(15.dp)),  border = BorderStroke(2.dp, Color(0xffED4956))
+            ) {
+
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+
+                        ) {
+                            Text(
+                                text = "Create your public place",
+                                color = Color(0xffed4956),
+                                fontWeight = FontWeight.Bold,
+                                style =  MaterialTheme.typography.subtitle1
+
+                            )
+                        }
+                    }
+                    Divider(thickness = 1.dp, color = Color(0xffED4956))
+                    Column(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp),
+                        verticalArrangement =Arrangement.SpaceEvenly)
+
+                    {
+
+                        Row(Modifier.fillMaxWidth().fillMaxHeight(0.2f), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                            Text(
+                                text = "Upload a place image",
+                                color = Color(0xffed4956),
+                                fontWeight = FontWeight.Bold,
+                                style =  MaterialTheme.typography.subtitle1
+
+                            )
+
+                        }
+                        Row(Modifier.fillMaxWidth().fillMaxHeight(1f),verticalAlignment = Alignment.Top, horizontalArrangement = Arrangement.Center) {
+                            Surface(
+                                modifier = Modifier
+                                    .size(150.dp)
+                                    .padding(5.dp),
+                                border = BorderStroke(width = 0.5.dp, color = Color(0xffed4956)),
+                                elevation = 4.dp,
+                            )
+                            {
+                                Image(
+                                    painter = painterResource(id = R.drawable.p_Img),
+                                    contentDescription = "",
+                                    modifier = Modifier.size(150.dp),
+                                    contentScale = ContentScale.Crop
+                                )
+                            }
+                        }
+                    }
+
+
+
+                }
+            }
         }
     }
 }
