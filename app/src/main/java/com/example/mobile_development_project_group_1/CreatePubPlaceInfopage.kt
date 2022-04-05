50580e6e8d1ab1f4f0bc4247088fc53a111796fa
Frank <chenmaofrank1996@gmail.com>
2022-04-05 18:06:58 +0300

    add new outlineButton

--- app/src/main/java/com/example/mobile_development_project_group_1/CreatePubPlaceInfopage.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/CreatePubPlaceInfopage.kt
@@ -28,6 +28,18 @@ fun CreatePubPlaceInfo(nav: NavHostController) {
     var title  = remember {
         mutableStateOf("")
     }
+    var workDays  = remember {
+        mutableStateOf("")
+    }
+    var weekend  = remember {
+        mutableStateOf("")
+    }
+    var contactInfo  = remember {
+        mutableStateOf("")
+    }
+    var webLink  = remember {
+        mutableStateOf("")
+    }
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally ) {
@@ -94,7 +106,18 @@ fun CreatePubPlaceInfo(nav: NavHostController) {
                     )
 
                 {
-
+                    MakeOutlineButtonForPublicPlace(title,"Public place name","bar",R.drawable.ic_title,
+                        KeyboardType.Text)
+                    MakeOutlineButtonForPublicPlace( workDays,"Workdays","16-04",R.drawable.ic_workdays,
+                        KeyboardType.Text)
+                    MakeOutlineButtonForPublicPlace(weekend,"Weekend","16-02",R.drawable.ic_workdays,
+                        KeyboardType.Text)
+                    MakeOutlineButtonForPublicPlace(contactInfo,"Contact us","Email/Phone Number",
+                        R.drawable.ic_contact,
+                        KeyboardType.Text)
+                    MakeOutlineButtonForPublicPlace(webLink,"Web link","http//:www.bar.com/",
+                        R.drawable.ic_baseline_link_24,
+                        KeyboardType.Text)
 
                 }
 
