3d40db26d883191d76e1e6483817f35f4ddd11bb
Diana <c0sydi00@students.oamk.fi>
2022-04-19 15:43:36 +0300

    description and events part are created

--- app/src/main/java/com/example/mobile_development_project_group_1/detailView.kt
+++ app/src/main/java/com/example/mobile_development_project_group_1/detailView.kt
@@ -1,5 +1,6 @@
 package com.example.mobile_development_project_group_1
 
+import android.graphics.fonts.FontFamily
 import android.widget.ScrollView
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.*
@@ -9,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -20,10 +22,14 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily.Companion.Monospace
+import androidx.compose.ui.text.font.FontFamily.Companion.SansSerif
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-
-
-
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -64,99 +70,177 @@ fun DetailView() {
         } // Back to HomePage button
         Column() {
             Column() {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround,
+
+
+                ) {
+                    Text(
+                        modifier = Modifier.paddingFromBaseline(0.dp,10.dp),
+                        text = "Bar name",
+                        fontWeight = FontWeight.ExtraBold,
+                        textAlign = TextAlign.Center,
+                        fontSize = 30.sp
+                    )
+                }
                 Row() {
-//                  Column(
-//                      modifier = Modifier
-//                          .width(200.dp)
-//                          .height(100.dp)
-//                          .border(1.dp, Color(0xffed4956), RoundedCornerShape(50.dp))
-//                  ){
-                      Image(
-                          modifier = Modifier
-                              .clip(CircleShape)
-                              .width(200.dp)
-                              .height(150.dp)
-                              .border(1.dp, Color(0xffed4956)),
+                    Image(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .width(200.dp)
+                            .height(150.dp)
+                            .border(1.dp, Color(0xffed4956)),
 
-                          painter = painterResource(R.drawable.ic_bar),
-                          contentDescription = "image",
-                      )
-//                  }
-               Column (
-                   modifier = Modifier
-                       .padding(3.dp)
-                       .width(200.dp)
-                       .height(150.dp)
-                       .background(
-                           color = Color(0xffed4956),
-                           shape = RoundedCornerShape(50.dp)
-                       ),
-                   horizontalAlignment = Alignment.CenterHorizontally,
-               verticalArrangement = Arrangement.SpaceAround
+                        painter = painterResource(R.drawable.ic_bar),
+                        contentDescription = "image",
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .width(200.dp)
+                            .height(150.dp)
+                            .background(
+                                color = Color(0xffed4956),
+                                shape = RoundedCornerShape(50.dp)
+                            ),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceAround
 
-                       ){
+                    ) {
 
-                   Text(text = "Opening hours",
-                       color = Color.White)
-                   Text(text = "Address",
-                       color = Color.White)
-                   Text(text = "Contact us",
-                       color = Color.White)
-                   Text(text = "Web link",
-                       color = Color.White)
-               }
+                        Text(
+                            text = "Opening hours",
+                            color = Color.White
+                        )
+                        Text(
+                            text = "Address",
+                            color = Color.White
+                        )
+                        Text(
+                            text = "Contact us",
+                            color = Color.White
+                        )
+                        Text(
+                            text = "Web link",
+                            color = Color.White
+                        )
+                    }
 
                 }
                 Column(
                     modifier = Modifier
-                        .verticalScroll(state= scrollState )
+                        .verticalScroll(state = scrollState)
                 ) {
 
-                Column (
-                    modifier = Modifier
-                        .padding(3.dp)
-                        .fillMaxWidth()
+                    Column(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
 
-                        .background(
+                            .background(
+                                color = Color.Transparent,
+                                shape = RoundedCornerShape(50.dp)
+                            )
+//                            .border(1.dp,
+//                                Color(0xffed4956),
+//                                shape = RoundedCornerShape(50.dp)),
+                        ,
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ) {
+
+                        Text(
+                            modifier = Modifier
+                                .border(
+                                    1.dp,
+                                    Color(0xffed4956),
+                                    shape = RoundedCornerShape(50.dp)
+                                )
+                                .fillMaxWidth(),
+                            text = "Description",
                             color = Color(0xffed4956),
-                            shape = RoundedCornerShape(50.dp)
-                        ),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "This is an amazing bar. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                        )
+                    }
+                    repeat(10) {
+                        Column(
+                            modifier = Modifier
+                                .border(1.dp, Color(0xffed4956))
+                                .fillMaxWidth()
+                                .background(
+                                    color = Color.White,
+                                    shape = RoundedCornerShape(50.dp),
+                                )
+                                .padding(5.dp),
+                            verticalArrangement = Arrangement.SpaceBetween
 
-                ){
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
 
-                    Text(
-                        text = "Description",
-                    color = Color.White,
-                    )
-                    Text (text="This is an amazing bar. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                    )
+                            ) {
+
+                                Column(modifier = Modifier.width(70.dp)) {
+
+                                    Text(
+                                        "22/10/2022",
+                                        textAlign = TextAlign.Center,
+                                        maxLines = 1,
+                                        fontSize = 13.sp,
+                                        fontFamily = SansSerif,
+
+                                        )
+                                }
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Text(
+                                        text = "Event 1",
+                                        textDecoration = TextDecoration.Underline,
+                                        fontWeight = FontWeight.Bold
+                                    )
+
+                                    Column(modifier = Modifier.width(250.dp)) {
+
+                                        Text(
+                                            "This is an amazing bar. Lorem Ipsum is simply dummy text of the printing and typesett",
+                                            textAlign = TextAlign.Center,
+                                            maxLines = 2,
+//                                    overflow = TextOverflow.Visible
+                                        )
+                                    }
+                                }
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.SpaceAround,
+                                    modifier = Modifier.width(60.dp)
+                                ) {
+                                    Text(
+                                        text = "17-20",
+                                        textAlign = TextAlign.Center
+                                    )
+                                    Column() {
+
+                                        Text(
+                                            "10$",
+                                            fontWeight = FontWeight.Bold,
+                                            textAlign = TextAlign.Center
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-             Column(modifier = Modifier
-                 .border(1.dp, Color(0xffed4956))
-                 .fillMaxWidth()
-                 .background(
-                     color = Color.White,
-                     shape = RoundedCornerShape(50.dp),
-                 )
 
-                ) {
-                 Column(
-//                     verticalAlignment = Alignment.CenterVertically,
-//                     horizontalArrangement = Arrangement.Center
-                 ) {
-//                     repeat(10) {
-//                         Text(text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
-//                         Text(text = "12-16")
-//
-//                     }
-                     repeat(10) {
-                         Text(text = "10$")
-                     }
-                 }
-             }
-
-                }
             }
         }
     }
